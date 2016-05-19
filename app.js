834a0e4451fa93eee7364103dd3940037b30439e
Matt Laws <mattlaws82@yahoo.com>
2016-05-19 20:26:16 +0100

    Finish!

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
-/*--------------------------------
-
-* Document ready
-
----------------------------------*/
+//-----------------------------------------------
+//   DOCUMENT READY
+//-----------------------------------------------
 
 $(document).ready(function() {
-	$('.js-like').on('click', function(event) {
-		event.preventDefault();
 
-		$(this).text('Liked!')
-		.closest('.news-item')
-		.addClass('is-liked');
-	});
+  //-----------------------------------------------
+  //   Like
+  //-----------------------------------------------
 
-	$('.js-add-link').on('click', function(event) {
-		event.preventDefault();
+  $('.js-like').on('click', function(event) {
+    event.preventDefault();
 
-		$('.js-form').toggleClass('is-visible');
+    $(this).text('Liked!')
+     .closest('.news-item')
+     .addClass('is-liked');
+  });
 
-	});
+  //-----------------------------------------------
+  //   Add Link
+  //-----------------------------------------------
+
+  $('.js-add-link').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-form').toggleClass('is-visible');
+  });
+
+  //-----------------------------------------------
+  //   Modal
+  //-----------------------------------------------
+
+  $('.js-show-modal').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-modal').addClass('is-visible');
+    $('.js-modal-overlay').addClass('is-visible');
+  });
+
+  $('.js-modal-overlay').on('click', function(event) {
+    $('.js-modal').removeClass('is-visible');
+    $('.js-modal-overlay').removeClass('is-visible');
+  });
 });
